a69b476cc85ffc73d9562f411f5d0f6f1fc0a397
Eric Zhiqiang Ma <eric@zhiqiangma.com>
2014-02-06 14:10:56 +0800

    only allow memcmp_se to be used after defining ENABLE_MEMCMP_SE; otherwise, a compilation error printed

--- libi0/string.h
+++ libi0/string.h
@@ -3,8 +3,8 @@
 
 #include "stddef.h"
 
-// NOTE: n must be 8*N
-// better strncmp function
+// NOTE: n must be 8*N, use b:sl to optimize
+// Consider using memcmp which is more efficient
 // TODO: use register to optimize
 long strncmp(char *s1, char *s2, long n)
 {
@@ -155,9 +155,8 @@ void memcpy(char* src, long len, char* dst)
     return;
 }
 
-// memcmp with side effect: registers are changed
-// faster but need to save the registers by the program
-long memcmp_se(char* s1, char* s2, long n)
+// for internal implementation only for memcmp_se
+long __memcmp_se(char* s1, char* s2, long n)
 {
     long ret_value;
 
@@ -172,8 +171,17 @@ long memcmp_se(char* s1, char* s2, long n)
     return ret_value;
 }
 
+// To use this function, the program should define 
+// ENABLE_MEMCMP_SE
+// before #include "libi0/string.h"; otherwise, the compilation fails.
+// memcmp with side effect: registers are changed
+// faster but need to save the registers by the program
+#ifdef ENABLE_MEMCMP_SE
+#define memcmp_se(s1, s2, n) __memcmp_se(s1, s2, n)
+#else
+#define memcmp_se(s1, s2, n) _Pragma ("GCC error \"To use memcmp_se, you must define ENABLE_MEMCMP_SE to indicate that you are aware its side effects.\"")
+#endif
 
-// Note: use asm strcmp to optimize
 long memcmp(char* s1, char* s2, long n)
 {
     long ret_value;
