34a38ff2b77bf8b1b53fef3c4d08c9fcfa8f3246
Xiang Gao <xgao1023@gmail.com>
2014-05-30 12:42:32 +0800

    Add feature for linking against static libc/msvcrt.
    * CMakeLists.txt now handle a variable CC0_USE_STATIC_CRT to indicate
      whether to link against static C/C++ runtime library, on UNIX-based
      platforms it is libc and libstdc++, while on Windows it is Microsoft
      VC++ Redistributes(msvcrt.dll, msvcrtd.dll, etc.).
    
    * When setting compiler variables always honor what is already there,
      so either append or replace the flags you want to modify, instead of
      setting it as a whole.
    
    * Also I handle 5 standard build types in CMake - None, Debug, Release,
      RelWithDebInfo, MinSizeRel. It is defined in standard CMake variable
      CMAKE_BUILD_TYPE.
    
    * P.S., To set CMake variables, pass -D<VarName>=<Value> to cmake
      command line, e.g.
          cmake -DCMAKE_BUILD_TYPE=Release -DCC0_USE_STATIC_CRT=TRUE ..
      It will build a release version linking against static CRT.

--- src/build-cc0.sh
+++ src/build-cc0.sh
@@ -6,7 +6,15 @@ mkdir -p $dir/toolchain/build/
 
 cd $dir/toolchain/build/
 
-cmake ../
+# Accept an optional command line parameter to set the build type.
+# Valid build types are Debug, Release, RelWithDebInfo, MinSizeRel
+if [ -z "$1" ]; then
+	cmake -DCMAKE_BUILD_TYPE=$1 -DCC0_USE_STATIC_CRT=1 ..
+else
+	cmake -DCC0_USE_STATIC_CRT=1
+fi
+
+# make VERBOSE=1
 make
 
 cd -
